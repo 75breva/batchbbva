aebca5be9637ec361fab93530651960cf4d4c6d1
Miguel Arredondo Calderon <miguel.arredondo@neoris.com>
2025-02-04 12:59:48 +0100

    Eliminar duplicados

--- src/main/java/com/murex/processor/AriadnaProcessor.java
+++ src/main/java/com/murex/processor/AriadnaProcessor.java
@@ -6,14 +6,31 @@ import org.slf4j.LoggerFactory;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.stereotype.Component;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Component
 public class AriadnaProcessor implements ItemProcessor<Ariadna, Ariadna>{
 
+//    private static final Logger LOG = LoggerFactory.getLogger(MurexItemProcessor.class);
+//
+//    @Override
+//    public Ariadna process(Ariadna item) throws Exception {
+//        LOG.info("Processing Ariadna information: {}", item);
+//        return item;
+//    }
+
     private static final Logger LOG = LoggerFactory.getLogger(MurexItemProcessor.class);
+    private Set<String> uniqueValues = new HashSet<>();
 
     @Override
     public Ariadna process(Ariadna item) throws Exception {
         LOG.info("Processing Ariadna information: {}", item);
-        return item;
+        if (uniqueValues.add(item.getCodestr())) {
+            return item;
+        } else {
+            return null; // Return null to filter out duplicates
+        }
     }
 }
+
--- src/main/java/com/murex/processor/UniqueAriadnaWriter.java
+++ src/main/java/com/murex/processor/UniqueAriadnaWriter.java
@@ -15,6 +15,37 @@ import javax.sql.DataSource;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
+//@Component
+//public class UniqueAriadnaWriter extends JdbcBatchItemWriter<Ariadna> {
+//
+//    private static final Logger logger = LoggerFactory.getLogger(UniqueAriadnaWriter.class);
+//
+//    private final NamedParameterJdbcTemplate jdbcTemplate;
+//    private final AtomicInteger recordsInserted = new AtomicInteger(0); // Contador de registros
+//
+//    public UniqueAriadnaWriter(DataSource dataSource, NamedParameterJdbcTemplate jdbcTemplate) {
+//        this.jdbcTemplate = jdbcTemplate;
+//    }
+//
+//    @Override
+//    public void write(List<? extends Ariadna> items) throws Exception {
+//        super.write(items);
+//        int count = 0;
+//        for (Ariadna item : items) {
+//            if (item != null) {
+//                String sql = "INSERT INTO ariadna (codestr, producto, descripcion) " +
+//                             "VALUES (:codestr, :producto, :descripcion) " +
+//                             "ON DUPLICATE KEY UPDATE codestr = :codestr";
+//
+//                jdbcTemplate.update(sql,new BeanPropertySqlParameterSource(item));
+//                count++;
+//            }
+//        }
+//        logger.info("Number of records written to the database: {}", count);
+//    }
+//}
+
+
 @Component
 public class UniqueAriadnaWriter extends JdbcBatchItemWriter<Ariadna> {
 
@@ -26,52 +57,18 @@ public class UniqueAriadnaWriter extends JdbcBatchItemWriter<Ariadna> {
     public UniqueAriadnaWriter(DataSource dataSource, NamedParameterJdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
         setDataSource(dataSource);
-        setSql("INSERT IGNORE INTO fecproceso (fec_proceso) VALUES (:fec_proceso)");
+        setSql("INSERT IGNORE INTO ariadna (codestr, producto, descripcion) VALUES (:codestr, :producto, :descripcion)");
         setItemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>());
     }
 
     @Override
     public void write(List<? extends Ariadna> items) throws Exception {
         super.write(items);
-        int count = 0;
-        for (Ariadna item : items) {
-            if (item != null) {
-                String sql = "INSERT INTO ariadna (codestr, producto, descripcion) " +
-                             "VALUES (:codestr, :producto, :descripcion) " +
-                             "ON DUPLICATE KEY UPDATE codestr = :codestr";
-
-                jdbcTemplate.update(sql,new BeanPropertySqlParameterSource(item));
-                count++;
-            }
-        }
-        logger.info("Number of records written to the database: {}", count);
+        int insertedCount = items.size();
+        recordsInserted.addAndGet(insertedCount);
+        logger.info("Se han insertado {} registros en este batch. Total acumulado: {}", insertedCount, recordsInserted.get());
     }
+
 }
-
-
-//@Component
-//public class UniqueAriadnaWriter extends JdbcBatchItemWriter<Ariadna> {
-//
-//    private static final Logger logger = LoggerFactory.getLogger(UniqueAriadnaWriter.class);
-//
-//    private final NamedParameterJdbcTemplate jdbcTemplate;
-//    private final AtomicInteger recordsInserted = new AtomicInteger(0); // Contador de registros
-//
-//    public UniqueAriadnaWriter(DataSource dataSource, NamedParameterJdbcTemplate jdbcTemplate) {
-//        this.jdbcTemplate = jdbcTemplate;
-//        setDataSource(dataSource);
-//        setSql("INSERT IGNORE INTO ariadna (codestr, producto, descripcion) VALUES (:codestr, :producto, :descripcion)");
-//        setItemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>());
-//    }
-//
-//    @Override
-//    public void write(List<? extends Ariadna> items) throws Exception {
-//        super.write(items);
-//        int insertedCount = items.size();
-//        recordsInserted.addAndGet(insertedCount);
-//        logger.info("Se han insertado {} registros en este batch. Total acumulado: {}", insertedCount, recordsInserted.get());
-//    }
-//
-//}
 
 
